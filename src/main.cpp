1a366ef1d29b0cc7a04f4a9c9d9947b9f719f494
Michael Gunlock <1138684+gunlock@users.noreply.github.com>
2026-03-29 14:51:59 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,35 +13,20 @@ PLUGIN_API int XPluginStart(char* outName, char* outSig, char* outDesc) {
   std::strncpy(outDesc, kPluginDesc, 256);
 
   XPLMDebugString("MyPlugin: started\n");
-
-  // TODO: Initialize ImGui context here
-  //   IMGUI_CHECKVERSION();
-  //   ImGui::CreateContext();
-  //   ImGui_ImplOpenGL3_Init("#version 330");
-
   return 1;
 }
 
 PLUGIN_API void XPluginStop() {
   XPLMDebugString("MyPlugin: stopped\n");
-
-  // TODO: Shutdown ImGui here
-  //   ImGui_ImplOpenGL3_Shutdown();
-  //   ImGui::DestroyContext();
 }
 
 PLUGIN_API int XPluginEnable() {
   XPLMDebugString("MyPlugin: enabled\n");
-
-  // TODO: Create X-Plane SDK window(s) and register draw callbacks here
-
   return 1;
 }
 
 PLUGIN_API void XPluginDisable() {
   XPLMDebugString("MyPlugin: disabled\n");
-
-  // TODO: Destroy X-Plane SDK window(s) here
 }
 
 PLUGIN_API void XPluginReceiveMessage(XPLMPluginID from, int msg, void* param) {
